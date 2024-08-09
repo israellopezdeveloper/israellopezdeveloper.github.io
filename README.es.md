5b0fcd6d53299e89632bda66dc2204bb6ee769d9
Israel López <israel.lopez.developer@gmail.com>
2024-08-09 13:33:30 +0200

    Feature/custom projects (#3)
    * fix
    
    * commit
    
    * commit
    
    * commit

---
+++ README.es.md
@@ -0,0 +1,59 @@
+# Portafolio Personal
+
+¡Bienvenido a mi portafolio! Este proyecto ha sido creado para mostrar mi trabajo y habilidades como desarrollador web. A continuación, encontrarás una descripción de cómo se ha construido este portafolio, incluyendo las tecnologías utilizadas y algunas de las características clave.
+
+## Tecnologías Utilizadas
+
+- **Next.js**: Este portafolio está construido con Next.js, un framework de React que permite la generación estática y el renderizado del lado del servidor, lo que garantiza un rendimiento rápido y una mejor experiencia de usuario. Next.js también facilita la optimización del SEO, algo crucial para un portafolio en línea.
+
+- **Blender**: Las animaciones 3D que se muestran en este portafolio fueron creadas utilizando Blender, un potente software de modelado 3D. Blender ha permitido diseñar modelos y escenas tridimensionales que luego se han integrado en la aplicación web.
+
+- **Three.js**: Para la implementación de las animaciones 3D en el navegador, he utilizado Three.js. Esta librería facilita la renderización de gráficos 3D en la web y ha sido fundamental para dar vida a las animaciones de Blender dentro de la aplicación Next.js.
+
+## Características
+
+- **Interfaz moderna y responsiva**: El portafolio está diseñado para ofrecer una experiencia de usuario fluida tanto en dispositivos móviles como en pantallas de escritorio.
+  
+- **Animaciones 3D**: Gracias a la combinación de Blender y Three.js, el portafolio cuenta con animaciones 3D interactivas que enriquecen la experiencia visual y demuestran mis habilidades en el desarrollo de gráficos avanzados.
+
+- **SEO optimizado**: Al utilizar Next.js, he podido optimizar el portafolio para motores de búsqueda, lo que mejora su visibilidad en línea y facilita que otros encuentren mi trabajo.
+
+## Cómo Ejecutar el Proyecto
+
+Si deseas ejecutar este proyecto localmente, sigue estos pasos:
+
+1. Clona este repositorio:
+
+```bash
+git clone https://github.com/israellopezdeveloper/israellopezdeveloper.github.io.git
+```
+
+2. Navega al directorio del Proyecto:
+
+
+```bash
+cd israellopezdeveloper.github.io.git
+```
+
+3. Instala las dependencias:
+
+```bash
+npm install
+```
+
+4. Inicia el servidor de desarrollo:
+
+```bash
+npm run dev
+```
+
+5. Abre tu navegador y visita `http://localhost:3000` para ver el portafolio en acción.
+
+## Contribuciones
+
+Este es un proyecto personal, pero si tienes sugerencias o encuentras algún problema, no dudes en abrir un issue o hacer un pull request. ¡Estoy abierto a mejoras y colaboraciones!
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT. Consulta el archivo [LICENSE](LICENSE) para más detalles.
+
--- pages/works.js
+++ pages/works.js
@@ -1,7 +1,7 @@
 import { Container, Heading, SimpleGrid, Divider, Box, WrapItem, Wrap, useColorModeValue } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
+import { ProjectGridItem, WorkGridItem } from '../components/grid-item'
 import { useContext, useEffect, useMemo, useState } from 'react'
 import { VoxelKoalaContext } from '../components/layouts/main'
 import TechBadge from '../components/techbadge'
@@ -11,6 +11,7 @@ import cvDataENS from '../data/CV.en.s.json'
 import cvDataES from '../data/CV.es.json'
 import cvDataESS from '../data/CV.es.s.json'
 import { useLanguage } from '../components/context/language_context'
+import axios from 'axios'
 
 function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
@@ -39,8 +40,91 @@ function useWindowSize() {
   return windowSize
 }
 
+async function fetchRepos() {
+  const test = false
+  if (!test) {
+    try {
+      const response = await axios.get('https://api.github.com/users/israellopezdeveloper/repos')
+      const repoData = response.data
+
+      const reposFilled = await Promise.all(
+        repoData.map(async (repo) => {
+          const startDate = new Date(repo.created_at)
+
+          // Fecha actual
+          const currentDate = new Date()
+
+          // Calcula la diferencia en años y meses
+          const yearsDiff = currentDate.getFullYear() - startDate.getFullYear()
+          const monthsDiff = currentDate.getMonth() - startDate.getMonth()
+
+          // Calcula el número total de meses
+          let totalMonths = yearsDiff * 12 + monthsDiff
+          totalMonths = (totalMonths === 0 ? 1 : totalMonths)
+          let techs = await axios.get(repo.languages_url)
+          techs = Object.keys(techs.data)
+          let techs2 = []
+          try {
+            techs2 = await axios.get(`https://raw.githubusercontent.com/${repo.owner.login}/${repo.name}/main/.techs`)
+            techs2 = techs2.data.split('\n')
+          } catch (e) {
+          }
+          techs = [...new Set([...techs, ...techs2])]
+          techs = techs.map((item) => {
+            return {
+              'tech': item,
+              'time': totalMonths
+            }
+          })
+          const titles = JSON.parse(repo.description)
+          const output = {
+            techs: techs,
+            title: {
+              es: titles.es.name,
+              en: titles.en.name
+            },
+            description: {
+              es: titles.es.desc,
+              en: titles.en.desc
+            },
+            url: repo.html_url,
+
+            thumbnail: `https://raw.githubusercontent.com/${repo.owner.login}/${repo.name}/main/.logo.png`
+          }
+          return output
+        })
+      )
+
+      return reposFilled
+    } catch (error) {
+      console.error(error)
+    } finally {
+    }
+  } else {
+    return [{
+      "techs": [
+        {
+          "tech": "JavaScript",
+          "time": 1
+        }
+      ], "title": {
+        "es": "Mi portfolio",
+        "en": "My portfolio"
+      }, "description": {
+        "es": "Código fuente de mi portfolio",
+        "en": "Source code of my portfolio"
+      }, "url": "https://github.com/israellopezdeveloper/israellopezdeveloper.github.io", thumbnail: "https://raw.githubusercontent.com/israellopezdeveloper/israellopezdeveloper.github.io/main/.logo.png"
+    }]
+  }
+}
+
+let repos = []
+
+fetchRepos().then(r => repos = r)
+
 const Works = () => {
   const voxel = useContext(VoxelKoalaContext)
+
   useEffect(() => {
     voxel.current.to_work()
   }, [voxel])
@@ -61,7 +145,7 @@ const Works = () => {
   const calculateTechnologyUsage = () => {
     const techUsage = {}
 
-    cvData.works.forEach(work => {
+    cvDataEN.works.forEach(work => {
       const period = work.period_time.split(' - ')
       const startDate = moment(period[0], 'MMMM YYYY')
       const endDate = moment(period[1], 'MMMM YYYY')
@@ -81,13 +165,22 @@ const Works = () => {
           }
         })
       })
+      repos.forEach(repo => {
+        repo.techs.forEach(tech => {
+          if (techUsage[tech.tech]) {
+            techUsage[tech.tech] += tech.time
+          } else {
+            techUsage[tech.tech] = tech.time
+          }
+        })
+      })
     })
 
     return techUsage
   }
 
   // Uso de useMemo para memorizar el uso de las tecnologías
-  const technologyUsage = useMemo(calculateTechnologyUsage, [cvData.works])
+  const technologyUsage = useMemo(calculateTechnologyUsage, [])
 
   // Estado para las tecnologías seleccionadas
   const [selectedTechnologies, setSelectedTechnologies] = useState(() => {
@@ -127,6 +220,11 @@ const Works = () => {
     })
   })
 
+  // Filtrar los proyectos basados en las tecnologías seleccionadas
+  const filteredProjects = repos.filter(repo => {
+    return repo.techs.map(t => t.tech).some(tech => selectedTechnologies[tech])
+  })
+
   const { width } = useWindowSize()
   const isMobile = width < 768; // Define el tamaño de pantalla móvil como < 768px
 
@@ -134,35 +232,38 @@ const Works = () => {
     <Layout title="Works">
       <Container display="flex" flexDirection={isMobile ? 'column' : 'row'} maxW={'100%'}>
         <Box flex="3">
-          <Heading as="h3" fontSize={20} mb={4}>
-            Works
-          </Heading>
+          <Section>
+            <Heading as="h3" fontSize={20} mb={4}>
+              Works
+            </Heading>
 
-          <SimpleGrid columns={[1, 1, 2]} gap={6}>
-            {filteredWorks.map((work, index) => (
-              <Section key={index}>
-                <WorkGridItem
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+              {filteredWorks.map((work, index) => (
+                <WorkGridItem key={index}
                   id={index.toString()}
                   title={work.name}
                   thumbnail={`/images/works/${work.thumbnail}`}>
                   {work.short_description}
                 </WorkGridItem>
-              </Section>
-            ))}
-          </SimpleGrid>
-
-          <Section delay={0.2}>
-            <Divider my={6} />
-            <Heading as="h3" fontSize={20} mb={4}>
-              Collaborations
-            </Heading>
+              ))}
+            </SimpleGrid>
           </Section>
 
-          <Section delay={0.4}>
-            <Divider my={6} />
+          <Divider my={6} />
+          <Section delay={0.2}>
             <Heading as="h3" fontSize={20} mb={4}>
-              Old works
+              Own projects
             </Heading>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+              {filteredProjects.map((repo) => (
+                <ProjectGridItem key={'Repo' + repo.id}
+                  id={repo.url}
+                  title={repo.title.es}
+                  thumbnail={repo.thumbnail}>
+                  {repo.description.es}
+                </ProjectGridItem>
+              ))}
+            </SimpleGrid>
           </Section>
         </Box>
 
