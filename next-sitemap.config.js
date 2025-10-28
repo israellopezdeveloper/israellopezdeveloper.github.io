47fb73473cb7b3e442e2c8f94c2b8ad617653ad1
Israel López <israel.lopez.developer@gmail.com>
2025-10-28 21:59:28 +0400

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://israellopezdeveloper.github.io',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: [],
+};
